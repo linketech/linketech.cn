a386c215f119b036bb02806bdf3a44831043ab7a
ETBox <13318972468@126.com>
2019-11-29 13:05:51 +0800

    feat: add build in git

---
+++ admin/build/precache-manifest.2e9959962d0e39809819e4140f3847c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7745145262cc69077d4b68f5a296cb",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "8b879b0cf07556c647fc",
+    "url": "/admin/static/css/2.4307843f.chunk.css"
+  },
+  {
+    "revision": "58217ce4505263c0af84",
+    "url": "/admin/static/css/main.c4af77ec.chunk.css"
+  },
+  {
+    "revision": "8b879b0cf07556c647fc",
+    "url": "/admin/static/js/2.89276458.chunk.js"
+  },
+  {
+    "revision": "58217ce4505263c0af84",
+    "url": "/admin/static/js/main.49ab0f5c.chunk.js"
+  },
+  {
+    "revision": "5c7ee73211bd5bca7053",
+    "url": "/admin/static/js/runtime-main.26471da4.js"
+  },
+  {
+    "revision": "2a73102059ab938618252faacbb7ba33",
+    "url": "/admin/static/media/logo2.2a731020.png"
+  }
+]);
