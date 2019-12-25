ccdf3fabe77dc6601b40e6a737f30c88cdc11b8a
xGreyLee <13160675187@163.com>
2019-12-25 17:23:03 +0800

    fix wrapper bug

--- admin/src/util/func-handler.js
+++ admin/src/util/func-handler.js
@@ -1,6 +1,6 @@
 const wrapperPromiseFunc = (fn) => {
 	return new Promise(resolve =>
-		resolve(fn.apply(this, this.arguments))).catch(e => console.error(e.stack))
+		resolve(fn())).catch(e => console.error(e.stack))
 }
 
 const wrapperFetch = async (url, options = { method: 'GET' }) => {
